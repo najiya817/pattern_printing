94ba6bae48dd6a66af14f86251086a1339dadee5
NAJIYA NASRIN <110896779+najiya817@users.noreply.github.com>
2022-08-29 14:11:07 +0530

    message

---
+++ basic.py
@@ -0,0 +1,9 @@
+for i in range(0,5):
+    for j in range(0,i+1):
+        print("*",end="")
+    print("\r")
+print("--------------------------")
+for i in range(0,5):
+    for j in range(0,5):
+        print("*")
+    print("\r")
